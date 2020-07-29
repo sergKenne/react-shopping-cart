bedf21dce2e8f2528bcbb88187b48650653c868b
Kenne Serge <kenne_serge@mail.ru>
2020-07-30 02:54:46 +0300

    p

--- server.js
+++ server.js
@@ -97,34 +97,28 @@ app.post("/api/orders", async (req, res) => {
 
 //server static assets if in production
 
-// if(process.env.NODE_ENV === "production") {
-//     //set static folder
-//     app.use(express.static('build'));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname,  'build', 'index.html')); 
-//     });
-// }
-
-if (process.env.NODE_ENV === 'production') {
-  //set static folder
-    // const express = require('express');
-    // const favicon = require('express-favicon');
-    // const path = require('path');
-    // const port = process.env.PORT || 8080;
-    // const app = express();
-    
-}
-
-    app.use(favicon(__dirname + '/build/favicon.ico'));
-    // the __dirname is the current directory from where the script is running
+if(process.env.NODE_ENV === "production") {
+    //set static folder
     app.use(express.static(__dirname));
     app.use(express.static(path.join(__dirname, 'build')));
-    app.get('/ping', function (req, res) {
-        return res.send('pong');
+    //app.use(express.static('build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  'build', 'index.html')); 
     });
-    app.get('/*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
+}
+
+
+
+    // app.use(favicon(__dirname + '/build/favicon.ico'));
+    // // the __dirname is the current directory from where the script is running
+    // app.use(express.static(__dirname));
+    // app.use(express.static(path.join(__dirname, 'build')));
+    // app.get('/ping', function (req, res) {
+    //     return res.send('pong');
+    // });
+    // app.get('/*', function (req, res) {
+    //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    // });
 
 app.listen(PORT, ()=>{
     console.log(`server running on the ${PORT}`)

cbadfaae4be8c7628b2e218c169433e307e2d3ae
Kenne Serge <kenne_serge@mail.ru>
2020-07-29 22:53:40 +0300

    test

--- server.js
+++ server.js
@@ -97,16 +97,16 @@ app.post("/api/orders", async (req, res) => {
 
 if(process.env.NODE_ENV === "production") {
     //set static folder
-    app.use(express.static('build'));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html')); 
-    });
+    // app.use(express.static('client/build'));
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
+    // });
 
     //set static folder
-    // app.use(express.static('build'));
-    // app.get("*", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname,  'build', 'index.html')); 
-    // });
+    app.use(express.static('build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  'build', 'index.html')); 
+    });
 
 
 

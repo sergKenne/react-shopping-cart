312fd0f4ac1537f08131fa1a09536035c6dbb92e
Kenne Serge <kenne_serge@mail.ru>
2020-07-29 23:48:15 +0300

    1

--- server.js
+++ server.js
@@ -93,39 +93,12 @@ app.post("/api/orders", async (req, res) => {
 
 //server static assets if in production
 
-
-
 if(process.env.NODE_ENV === "production") {
-    //set static folder
-    // app.use(express.static('client/build'));
-    // app.get("*", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
-    // });
-
     //set static folder
     app.use(express.static('build'));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname,  'build', 'index.html')); 
     });
-
-
-
-
-    // app.use(favicon(__dirname + '/build/favicon.ico'));
-    // app.use(express.static('build'));
-    // //app.use(express.static(__dirname));
-    // //app.use(express.static(path.join(__dirname, 'build')));
-    // app.get('/ping', function (req, res) {
-    // return res.send('pong');
-    // });
-    // app.get('/*', function (req, res) {
-    // res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    // });
-
-    // app.use('/', express.static(__dirname + '/build'));
-    // app.get('/', (req, res) => res.sendFile(__dirname + '/build/index.html'));
-
-
 }
 
 app.listen(PORT, ()=>{

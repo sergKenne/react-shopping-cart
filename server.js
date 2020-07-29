aaac475ed3cc7f4a38c0aad59515c1d4650c4811
Kenne Serge <kenne_serge@mail.ru>
2020-07-29 13:06:05 +0300

    deploy to heroku

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require('express');
+
+//
+// const favicon = require('express-favicon');
+// const path = require('path');
+
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const shortid = require('shortid');
 var cors = require('cors');
 var app = express();
 
+//
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/ping', function (req, res) {
+//   return res.send('pong');
+// });
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/shopping-card-db", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+const { MONGOURI } = require('./config/keys');
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect( MONGOURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
@@ -84,8 +104,6 @@ app.post("/api/orders", async (req, res) => {
     res.send(order);
 });
 
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
     console.log(`server running on the ${PORT}`)
 })

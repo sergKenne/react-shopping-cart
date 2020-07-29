2275ae6abae23a91d499bf71d10fe3154f02bd12
Kenne Serge <kenne_serge@mail.ru>
2020-07-29 14:36:01 +0300

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 //
-// const favicon = require('express-favicon');
-// const path = require('path');
+const favicon = require('express-favicon');
+const path = require('path');
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -103,6 +103,30 @@ app.post("/api/orders", async (req, res) => {
     const order = await Order(req.body).save();
     res.send(order);
 });
+
+
+//server static assets if in production
+
+if(process.env.NODE_ENV === "production") {
+    //set static folder
+    // app.use(express.static('client/build'));
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
+    // });
+
+
+    app.use(favicon(__dirname + '/build/favicon.ico'));
+    app.use(express.static(__dirname));
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/ping', function (req, res) {
+    return res.send('pong');
+    });
+    app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+
+
+}
 
 app.listen(PORT, ()=>{
     console.log(`server running on the ${PORT}`)

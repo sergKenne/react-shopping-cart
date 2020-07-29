bc0e09b7ab8e31ffec01b05af93869e116b42dc6
Kenne Serge <kenne_serge@mail.ru>
2020-07-29 16:03:40 +0300

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-
-//
 const favicon = require('express-favicon');
 const path = require('path');
 
@@ -9,18 +7,6 @@ const bodyParser = require('body-parser');
 const shortid = require('shortid');
 var cors = require('cors');
 var app = express();
-
-//
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/ping', function (req, res) {
-//   return res.send('pong');
-// });
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
 
 app.use(cors());
 app.use(bodyParser.json());

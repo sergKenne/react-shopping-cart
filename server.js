a10792cb0822e8a74899b635da7fa9d9e8d22045
Kenne Serge <kenne_serge@mail.ru>
2020-07-30 10:32:36 +0300

    make it better

--- server.js
+++ server.js
@@ -94,16 +94,16 @@ app.post("/api/orders", async (req, res) => {
     res.send(order);
 });
 
-
 //server static assets if in production
 
 if(process.env.NODE_ENV === "production") {
     //set static folder
-    app.use(express.static('build'));
+    app.use(express.static('client/build'));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  'build', 'index.html')); 
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
     });
 }
+
 
 app.listen(PORT, ()=>{
     console.log(`server running on the ${PORT}`)
